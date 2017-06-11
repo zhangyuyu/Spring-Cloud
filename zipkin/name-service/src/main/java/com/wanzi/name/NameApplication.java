7bdfa30625d27c2f9db6c8d9b91db8e4f073d5b2
zhangyu <1580074674@qq.com>
2017-06-11 22:00:56 +0800

    [Yu] Spring Cloud Zipkin Demo.

--- zipkin/name-service/src/main/java/com/wanzi/name/NameApplication.java
+++ zipkin/name-service/src/main/java/com/wanzi/name/NameApplication.java
@@ -1,16 +1,14 @@
-package com.wanzi.user;
+package com.wanzi.name;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class UserApplication {
+public class NameApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(NameApplication.class, args);
 	}
 }
