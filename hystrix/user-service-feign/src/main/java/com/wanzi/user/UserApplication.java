b911ebfdc7157e9fe6c7c3124be36a4dcc80634e
zhangyu <1580074674@qq.com>
2017-06-11 11:22:13 +0800

    [Yu] Spring Cloud Consul Demo.
    [Yu] Spring Cloud Hystrix Demo.
    
    [Yu] Spring Cloud Hystrix Demo.
    
    [Yu] Spring Cloud Hystrix Demo.

---
+++ hystrix/user-service-feign/src/main/java/com/wanzi/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.wanzi.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
