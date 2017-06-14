b0775374803252b499659770d9ce996a8bbef9d9
zhangyu <1580074674@qq.com>
2017-06-14 21:10:11 +0800

    [Yu] remove registrator from docker-compose file as services will register themselves to consul.

---
+++ micro-service/README.md
@@ -0,0 +1,8 @@
+Spring Cloud Micro Service with Docker
+--
+## 启动
+1. 运行启动脚本
+ `. ./up.sh` 或者`source ./up.sh`之后执行`.up.sh`
+2. 访问consul ui
+`http://localhost:8500/ui/`
+3. 单独访问`name-service`和`user-service`
--- micro-service/up.sh
+++ micro-service/up.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+blue=`tput setaf 6`
+
+#export CONSUL_HOST=`ifconfig  | grep inet | awk '{print $2}' | cut -d "/" -f 1 | tail -n 1`
+export CONSUL_HOST=192.168.33.1
+echo "${blue}==>Setting environment variable"
+echo "Consul host is ${CONSUL_HOST}"
+
+echo "${blue}==>Stopping running container"
+docker-compose stop
+
+echo "${blue}==>Removing stopped container"
+docker-compose rm -f
+
 pushd ./user-service
   ./gradlew clean build
   chmod 777 ./build/libs/*.jar
@@ -10,3 +23,4 @@ pushd ./name-service
   chmod 777 ./build/libs/*.jar
 popd
 
+docker-compose up
