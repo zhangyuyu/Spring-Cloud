647aaa767863bf71b8aaa0a8119a5009e2930a92
zhangyu <1580074674@qq.com>
2017-06-14 21:28:53 +0800

    [Yu] set consul_host as system variable in the up.sh

--- micro-service/name-service/src/main/java/com/wanzi/name/NameApplication.java
+++ micro-service/name-service/src/main/java/com/wanzi/name/NameApplication.java
@@ -3,12 +3,21 @@ package com.wanzi.name;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NameApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NameApplication.class, args);
+		SpringApplication application = new SpringApplication(NameApplication.class);
+		application.setEnvironment(consulEnvironment());
+		application.run(args);
+	}
+
+	private static StandardEnvironment consulEnvironment() {
+		StandardEnvironment environment = new StandardEnvironment();
+		environment.getSystemProperties().put("spring.cloud.consul.host", System.getenv("CONSUL_HOST"));
+		return environment;
 	}
 }
